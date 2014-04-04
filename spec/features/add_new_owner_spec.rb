b7de9ee3c1fd31ed02971cc8fd3034c4668db259
laransom <laransom@gmail.com>
2014-04-04 15:16:31 -0400

    added owner model

--- spec/features/add_new_owner_spec.rb
+++ spec/features/add_new_owner_spec.rb
@@ -9,6 +9,17 @@ feature 'add a new owner record', %q{
   scenario 'Successfully registers new owner with valid attributes' do
     pre_count = Owner.count
     visit new_owner_path
+
+    fill_in :first_name, with: 'Sterling'
+    fill_in :last_name, with: 'Archer'
+    fill_in :email, with: 'valid@email.com'
+    fill_in :company_name, with: 'Isis'
+
+    click_on 'Add Owner'
+
+    expect(page).to have_content 'Owner was succesfully added'
+    expect(page).to have_content 'Sterling Archer'
+    expect(Owner.count).to eq(pre_count + 1)
   end
 
 end
